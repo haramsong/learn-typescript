2eaa4bd18857346ccdc379a441dd02f5712a6a99
= <=>
2023-10-05 16:28:59 +0900

    1005_학습

---
+++ class-note/2_functions.ts
@@ -0,0 +1,28 @@
+// 함수의 파라미터에 타입을 정의하는 방식
+// function sum(a: number, b: number) {
+//     return a + b;
+// }
+
+// // function sum(a: number, b: number): number
+// sum(10, 20);
+
+// 함수의 반환 값에 타입을 정의하는 방식
+function add(): number {
+    return 10;
+}
+
+// 함수에 타입을 정의하는 방식
+// function sum(a: number, b: number): number {
+//     return a + b;
+// }
+
+// Expected 0-2 arguments, but got 4.ts(2554)
+// sum(10, 20, 30, 40);
+
+// 함수의 옵셔널 파라미터
+function log(a: string, b?: string) {
+
+}
+log('hello world');
+log('hello ts', 'abc');
+
--- why-ts/app.js
+++ why-ts/app.js
@@ -9,13 +9,38 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+/**
+ * @typedef {object} Address 
+ * @property {string} street
+ * @property {string} email
+ * @property {string} address
+ */
+
+/**
+ * @typedef {object} User 
+ * @property {string} name
+ * @property {string} email
+ * @property {string} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+
 function startApp() {
-  axios
-    .get(url)
+  // axios
+  //   .get(url)
+  fetchUser()
     .then(function (response) {
-      console.log(response);
       user = response.data;
+      console.log(user);
       // TODO: 이름, 이메일, 주소 표시하기
+      username.innerText = user[0].name;
+      email.innerText = user[0].email;
+      address.innerText = user[0].addres;
     })
     .catch(function (error) {
       console.log(error);
